45908f11b99debe55be47d0233aca52e145a5c46
Kresa12 <aurakresa12@gmail.com>
2025-09-26 23:10:49 +0700

    mobile-kotlin: installing android studio and open

--- mobile-kotlin.sh
+++ mobile-kotlin.sh
@@ -6,4 +6,10 @@ echo "======================================="
 # Update repo
 sudo apt update -y
 
-echo ">> Instalasi Mobile Kotlin selesai ✅"
+echo ">> Menginstall Android Studio..."
+sudo snap install android-studio --classic
+
+echo ">> Membuka Android Studio dan Discord..."
+nohup android-studio >/dev/null 2>&1 &
+
+echo ">> Instalasi Mobile Kotlin selesai"
