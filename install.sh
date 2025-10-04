908adebd4c5d49cc6d700cc2cf56d4f395c91cf0
Kresa12 <aurakresa12@gmail.com>
2025-10-04 20:58:14 +0700

    add laravel in install.sh

--- install.sh
+++ install.sh
@@ -11,11 +11,12 @@ echo "1) Mobile Kotlin"
 echo "2) Mobile Flutter"
 echo "3) Frontend React"
 echo "4) Backend Express"
-echo "5) Keluar"
+echo "5) Laravel"
+echo "6) Keluar"
 echo "======================================="
 
 # Pakai /dev/tty supaya bisa input walau dijalankan via pipe
-read -p "Masukkan pilihan (1/2/3/4/5): " choice </dev/tty
+read -p "Masukkan pilihan (1/2/3/4/5/6): " choice </dev/tty
 
 case $choice in
   1)
@@ -35,6 +36,10 @@ case $choice in
     curl -s $BASE_URL/backend-express.sh | bash
     ;;
   5)
+    echo ">> Download & jalankan installer Laravel..."
+    curl -s $BASE_URL/laravel.sh | bash
+    ;;
+  6)
     echo "Keluar."
     exit 0
     ;;
